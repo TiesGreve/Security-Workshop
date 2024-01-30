22df1a5d4faf2d721ae3002e39e61fa00942882e
TiesGreve <113978745+TiesGreve@users.noreply.github.com>
2024-01-30 09:16:22 +0100

    Add project files.

---
+++ MooiereNaam/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MooiereNaam.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
